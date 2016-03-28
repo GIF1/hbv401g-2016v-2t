f43eb7b8a872947b6d6cd7e02b90cb4c8cee33d7
Daníel Freyr Hjartarson <Hjartarson92@gmail.com>
2016-03-28 15:35:52 +0000

    Implemented a mock object for the hotel class

--- src/metaSearchEngine/mockobjects/Hotel.java
+++ src/metaSearchEngine/mockobjects/Hotel.java
@@ -1,25 +1,39 @@
 package metaSearchEngine.mockobjects;
 
-import java.util.*;
-
 public class Hotel {
 
 	private int numRoomSingleAvail;
+	private int numRoomDoubleAvail;
+	private int numRoomMultiAvail;
+	private int priceSingle;
+	private int priceDouble;
+	private int priceMulti;
+	private String Location;
 
-	public Hotel(String flightnr, String depTime, String depLoc, String arrivLoc, int price_kr, ArrayList<String> sellerInfo) {
-		flightNr = flightnr;
-		departureTime = depTime;
-		departureLoc = depLoc;
-		arrivalLoc = arrivLoc;
-		price = price_kr;
-		dealerInfo = sellerInfo;
+	public Hotel(int numSingle, int numDouble, int numMulti, int priceSingle, int priceDouble, int priceMulti, String Loc) {
+		this.numRoomSingleAvail = numSingle;
+		this.numRoomDoubleAvail = numDouble;
+		this.numRoomMultiAvail = numMulti;
+		this.priceSingle = priceSingle;
+		this.priceDouble = priceDouble;
+		this.priceMulti = priceMulti;
+		this.Location = Loc;
+		
 	}
 
-	// Get methods for all the attributes
-	public int get_price() {return price; }
-	public String get_depTime() { return departureTime; }
-	public String get_depLoc() { return departureLoc; }
-	public String get_arrivLoc() { return arrivalLoc; }
-	public ArrayList<String> get_dealerInfo() { return dealerInfo; }
-	public String get_flightNr() {return flightNr; }
+	public int getNumRoomSingleAvail() {return this.numRoomSingleAvail; }
+	public int getNumRoomDoubleAvail() {return this.numRoomDoubleAvail; }
+	public int getNumRoomMultiAvail() {return this.numRoomMultiAvail; }
+	public int getPriceSingle() {return this.priceSingle; }
+	public int getPriceDouble() {return this.priceDouble; }
+	public int getPriceMulti() {return this.priceMulti; }
+	public String getLocation() {return this.Location; }
+	
+	void setNumRoomSingleAvail (int numRoomsSingle) { this.numRoomSingleAvail = numRoomsSingle; }
+	void setNumRoomDoubleAvail (int numRoomsDouble) { this.numRoomDoubleAvail = numRoomsDouble; }
+	void setNumRoomMultiAvail (int numRoomsMulti) { this.numRoomMultiAvail = numRoomsMulti; }
+	void setPriceSingle (int priceSingle) { this.priceSingle = priceSingle; }
+	void setPriceDouble (int priceDouble) { this.priceDouble = priceDouble; }
+	void setPriceMulti (int priceMulti) { this.priceMulti = priceMulti; }
+	void setLocation (String Loc) {this.Location = Loc; }
 }
