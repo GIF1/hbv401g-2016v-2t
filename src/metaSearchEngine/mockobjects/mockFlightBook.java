40b8c83373008314b11849cb46686f6f6cddd4a1
GIF1 <gif1@hi.is>
2016-04-02 23:13:47 +0000

    Trying to commit, hard because of conflicts

---
+++ src/metaSearchEngine/mockobjects/mockFlightBook.java
@@ -0,0 +1,12 @@
+package metaSearchEngine.mockobjects;
+
+import metaSearchEngine.program.FlightBooking;
+
+public class mockFlightBook {
+	
+	public FlightBooking flight;
+	
+	public void FlightBook (FlightBooking flight) {
+		this.flight = flight;
+	}
+}
--- src/metaSearchEngine/test/test_cases_sprint4.java
+++ src/metaSearchEngine/test/test_cases_sprint4.java
@@ -1,44 +1,124 @@
 package metaSearchEngine.test;
 
-import metaSearchEngine.mockobjects.Flight;
+import metaSearchEngine.mockobjects.*;
 import metaSearchEngine.program.*;
 
-import java.util.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.instanceOf;
 
-//import static org.junit.Assert.*;
+import java.util.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+
 public class test_cases_sprint4 {
 	ArrayList<String> wow_info = new ArrayList<String>();
-	
+	FlightSearchCriteria newFlightSearch = new FlightSearchCriteria();
+		
+
 	@Before
 	public void setUp() {
 		wow_info.add("WOW air");
-		wow_info.add("KatrÌnart˙n 12 - 105 ReykjavÌk");
+		wow_info.add("Katr√≠nart√∫n 12 - 105 Reykjav√≠k");
 		wow_info.add("Kt. 451011-0220");
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		//Flight mittflug = null;
-		//FlightBooking prufubokun = null;
-		//ArrayList<String> wow_info = null;
+
 	}	
+	
 	@Test
 	public void setCorrectLuggage() {
-		Flight mittflug = new Flight("AAV321", "09:00 1.jan", "Akureyri", "ReykjavÌk", 12000, wow_info);
-		FlightBooking prufubokun = new FlightBooking(mittflug, "23E", "Sigurgeir");
-		prufubokun.setLuggage(3);
-		assertEquals(prufubokun.luggage, 3);
+		Flight flight = new FlightExtend("AA123", new Date(2016-1900,7+1,15,22,30), 
+				"Akureyri", new Date(2016-1900,7+1,15,0,30), "Reykjav√≠k", 12000,  
+				new String[]{"13A", "13B", "11A"}, new String[]{"1A"}, wow_info);
+		flight.set_seatNr(new String[]{"13A", "13B", "11A"});
+		FlightBooking testbooking = new FlightBooking(flight, 1, "Sigurgeir");
+		testbooking.setLuggage(3);
+		assertEquals(testbooking.luggage, 3);
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void setWrongLuggage() {
-		Flight mittflug = new Flight("AAV321", "09:00 1.jan", "Akureyri", "ReykjavÌk", 12000, wow_info);
-		FlightBooking prufubokun = new FlightBooking(mittflug, "23E", "Sigurgeir");
-		prufubokun.setLuggage(-3);
+		Flight flight = new FlightExtend("AA123", new Date(2016-1900,7+1,15,22,30), 
+				"Akureyri", new Date(2016-1900,7+1,15,0,30), "Reykjav√≠k", 12000,  
+				new String[]{"13A", "13B", "11A"}, new String[]{"1A"}, wow_info);
+		flight.set_seatNr(new String[]{"13A", "13B", "11A"});
+		FlightBooking testbooking = new FlightBooking(flight, 1, "Sigurgeir");
+		testbooking.setLuggage(-3);
 	}
+	
+	// Testing if an empty search criteria is stopped before calling the flight
+	// search engine. This check is performed in the SearchEngine class.
+	@Test(expected=IllegalArgumentException.class)
+	public void emptyFlightSearch() {
+		List<Flight> flightResults = SearchEngine.flightSearch(newFlightSearch);
+	}
+	
+	// Testing if a normal search is performed that the results is a List of
+	// class instances of FlightExtend, which extends the Flight class
+	// if there are any flights that match else it should be an empty List.
+	@Test
+	public void flightSearch() {
+		newFlightSearch.setDepartureTime(new Date(2016-1900,7+1,15));
+		newFlightSearch.setDepartureLoc("Akureyri");
+		newFlightSearch.setArrivalLoc("Reykjav√≠k");
+		newFlightSearch.setPriceRange(new int[]{10000,20000});
+		newFlightSearch.setReturnTrip(false);
+		newFlightSearch.setNumSeats(1);
+		newFlightSearch.setSeatClass("Economy");
+		List<Flight> flightResults = SearchEngine.flightSearch(newFlightSearch);
+		if (flightResults.size()>0) {
+			for (int i=0; i<flightResults.size(); i++) {
+				assertThat(flightResults.get(i), instanceOf(FlightExtend.class));
+			}
+		} else {
+			assertEquals(flightResults.size(),0);
+		}
+	}
+	
+	// Testing if a instance of a FlightBooking class is
+	// delivered to the flight search engine when
+	// flight is booked.
+	@Test
+	public void makeBooking1() {
+		newFlightSearch.setDepartureTime(new Date(2016-1900,7+1,15));
+		newFlightSearch.setDepartureLoc("Akureyri");
+		newFlightSearch.setArrivalLoc("Reykjav√≠k");
+		newFlightSearch.setPriceRange(new int[]{10000,20000});
+		newFlightSearch.setReturnTrip(false);
+		newFlightSearch.setNumSeats(1);
+		newFlightSearch.setSeatClass("Economy");
+		List<Flight> flightResults = SearchEngine.flightSearch(newFlightSearch);
+
+		FlightBooking testBooking = new FlightBooking(flightResults.get(0),1,"Gunnar");
+		mockFlightBook bookedFlight = testBooking.bookFlight();
+		
+		assertThat(bookedFlight.flight, instanceOf(FlightBooking.class));
+	}
+	
+	// Testing that the customer name from the Booking class
+	// that is extended by FlightBooking is included in the
+	// booking.
+	@Test
+	public void makeBooking2() {
+		newFlightSearch.setDepartureTime(new Date(2016-1900,7+1,15));
+		newFlightSearch.setDepartureLoc("Akureyri");
+		newFlightSearch.setArrivalLoc("Reykjav√≠k");
+		newFlightSearch.setPriceRange(new int[]{10000,20000});
+		newFlightSearch.setReturnTrip(false);
+		newFlightSearch.setNumSeats(1);
+		newFlightSearch.setSeatClass("Economy");
+		List<Flight> flightResults = SearchEngine.flightSearch(newFlightSearch);
+
+		FlightBooking testBooking = new FlightBooking(flightResults.get(0),1,"Gunnar");
+		mockFlightBook bookedFlight = testBooking.bookFlight();
+		
+		assertEquals(bookedFlight.flight.getCustomer(), "Gunnar");
+	}
+
 }
+
