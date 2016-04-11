3c6208a38dc5067dd5b91b2a82130068ef52401f
Daníel Freyr Hjartarson <Hjartarson92@gmail.com>
2016-04-11 22:15:10 +0000

    Updated to newest version

--- src/metaSearchEngine/program/UserClass.java
+++ src/metaSearchEngine/program/UserClass.java
@@ -1,3 +1,5 @@
+package metaSearchEngine.program;
+
 // Instances of classes that extend this interface are
 // User objects that contain information on customers seeking
 // various travel services. 
@@ -6,7 +8,7 @@ public interface UserClass {
 
 	// Minimum attributes all User classes should contain: 
 	private String username;
-	private String id;	// unique for database usage. 
+	private final String id;	// unique for database usage. Initialized in constructor. Never changed. 
 	private String email;
 
 	// Usage: username = changeUserName(newUserName);
@@ -14,16 +16,12 @@ public interface UserClass {
 	// 	   of length 3 <= username.length() <= 20 and 
 	//	   consist only of numbers and letters and start with a letter
 	// After: username has been changed to newUserName
-	void changeUserName(String newUserName)
-
-	// Usage: id = setId(newId);
-	// Before: newId contains a non-empty string. 
-	//	   id could contain null but otherwise a non-empty string
-	// After: id has been changed to newId
+	void setUserName(String newUserName);
 	
 	// Usage: email = setEmail(newEmail);
-	// Before: Both variables match the regular expression
-	//	   ^([\w−] + (? : \.[\w−]+)*)@((? : [\w−] + \.)*\w[\w−]0, 66)\.([a − z]2, 6(? : \.[a − z]2)?)$
+	// Before: Both variables are contain a valid email address.
+	// 	  (They might for example match the regular expression
+	//	   ^([\w−] + (? : \.[\w−]+)*)@((? : [\w−] + \.)*\w[\w−]0, 66)\.([a − z]2, 6(? : \.[a − z]2)?)$ )
 	// After: email has been changed to newEmail.
-	void setEmail(String newEmail)
+	void setEmail(String newEmail);
 }
